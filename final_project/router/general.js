7ef2a6968586a9a2feb308c3eab12b115206f279
grodri35 <gesrod2695@proton.me>
2025-07-14 15:40:47 -0400

    Completed Task 6: User registration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,20 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be inplemented"});
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+    }
+  
+    const userExists = users.some(user => user.username === username);
+  
+    if (userExists) {
+      return res.status(409).json({ message: "Username already exists" });
+    }
+  
+    users.push({ username, password });
+    return res.status(200).json({ message: "User registered successfully" });
 });
 
 // Get the book list available in the shop
@@ -65,8 +77,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+  
+    if (book) {
+      return res.status(200).json(book.reviews);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 module.exports.general = public_users;
